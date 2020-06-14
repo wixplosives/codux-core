ba976f031f20fa639103c7748783880a898675af
Avi Vahl <avi.vahl@wix.com>
2020-06-14 15:32:55 +0300

    infra: prettify json/md/yml with 2 spaces

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,13 @@
 module.exports = {
     printWidth: 120,
     singleQuote: true,
-    tabWidth: 4
-}
+    tabWidth: 4,
+    overrides: [
+        {
+            files: ['*.json', '*.yml', '*.md', '.eslintrc'],
+            options: {
+                tabWidth: 2,
+            },
+        },
+    ],
+};
