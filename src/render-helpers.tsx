12effed666fe04dbd1bda24d167d7e04b340466f
Nadav Abrahami <nadav@wix.com>
2021-04-18 15:30:31 +0300

    tests pass

--- src/render-helpers.tsx
+++ src/render-helpers.tsx
@@ -92,30 +92,44 @@ const applyStylesToCanvas = (canvas: HTMLDivElement, canvasEnvironmentProps: Par
         canvas.style[styleProperty] = stylePropertyValue;
     }
 };
-export type HookNames<DATA extends IGeneralMetaData<unknown, HookMap>> = keyof NonNullable<DATA['__hooks']> & string
+export type HookNames<DATA extends IGeneralMetaData<unknown, HookMap>> = keyof NonNullable<DATA['__hooks']> & string;
 
-export function getPluginsWithHooks<DATA extends IGeneralMetaData<unknown, HookMap>>(data: DATA, hookName: HookNames<DATA>): PluginInfo<IPROPS, DATA, Plugin<IPROPS, DATA>>[] {
+export function getPluginsWithHooks<DATA extends IGeneralMetaData<unknown, HookMap>>(
+    data: DATA,
+    hookName: HookNames<DATA>
+): PluginInfo<IPROPS, DATA, Plugin<IPROPS, DATA>>[] {
     if (!data.plugins) {
-        return []
+        return [];
     }
-    return data.plugins.filter(item => !!(item.key.plugin as HookMap)[hookName])
+    return data.plugins.filter((item) => !!(item.key.plugin as HookMap)[hookName]) as PluginInfo<
+        IPROPS,
+        DATA,
+        Plugin<IPROPS, DATA>
+    >[];
 }
 
+export type HookParams<DATA extends IGeneralMetaData<unknown, HookMap>, HOOK extends HookNames<DATA>> = NonNullable<
+    NonNullable<DATA['__hooks']>[HOOK]
+> extends (pluginParams: never, ...args: infer U) => void | unknown
+    ? U
+    : never;
 
-export type HookParams<DATA extends IGeneralMetaData<unknown, HookMap>, HOOK extends HookNames<DATA>> = NonNullable<NonNullable<DATA['__hooks']>[HOOK]> extends (pluginParams: never, ...args: infer U) => void | unknown ? U : never
-
-
-export function callHooks<DATA extends IGeneralMetaData<unknown, HookMap>, HOOKNAME extends HookNames<DATA>>(data: DATA, hookName: HOOKNAME, ...props: HookParams<DATA, HOOKNAME>): void {
+export function callHooks<DATA extends IGeneralMetaData<unknown, HookMap>, HOOKNAME extends HookNames<DATA>>(
+    data: DATA,
+    hookName: HOOKNAME,
+    ...props: HookParams<DATA, HOOKNAME>
+): void {
     const plugins = getPluginsWithHooks(data, hookName);
     for (const pluginInfo of plugins) {
-
         if ((pluginInfo.key.plugin as HookMap)[hookName]) {
-
-            ((pluginInfo.key.plugin as HookMap)[hookName] as unknown as HOOK<IPROPS, HookParams<DATA, HOOKNAME>, void>)(pluginInfo.props as never, ...props)
+            (((pluginInfo.key.plugin as HookMap)[hookName] as unknown) as HOOK<
+                IPROPS,
+                HookParams<DATA, HOOKNAME>,
+                void
+            >)(pluginInfo.props as never, ...props);
         }
     }
 }
-
 
 export const setupSimulationStage: SetupSimulationStage = (simulation) => {
     const canvas = document.createElement('div');
@@ -123,12 +137,12 @@ export const setupSimulationStage: SetupSimulationStage = (simulation) => {
 
     const resetWindow = applyStylesToWindow(simulation.environmentProps);
     applyStylesToCanvas(canvas, simulation.environmentProps);
-    callHooks(simulation, 'beforeAppendCanvas', canvas)
+    callHooks(simulation, 'beforeAppendCanvas', canvas);
 
     document.body.appendChild(canvas);
 
     const cleanup = () => {
-        callHooks(simulation, 'beforeStageCleanUp', canvas)
+        callHooks(simulation, 'beforeStageCleanUp', canvas);
 
         canvas.remove();
         resetWindow();
@@ -139,12 +153,12 @@ export const setupSimulationStage: SetupSimulationStage = (simulation) => {
 
 export const renderSimulation: RenderSimulation = (simulation) => {
     const { canvas, cleanup: stageCleanup } = setupSimulationStage(simulation);
-    simulation.renderer(canvas)
+    simulation.renderer(canvas);
 
     return {
         canvas,
         cleanup: (): void => {
-            simulation.cleanup(canvas)
+            simulation.cleanup(canvas);
             stageCleanup();
         },
     };
