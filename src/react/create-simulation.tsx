99f4e6a8eda88fc31bfde8b85011428a157ed49a
tomermes <tomerme@wix.com>
2021-04-18 13:00:21 +0300

    fix typecheck

--- src/react/create-simulation.tsx
+++ src/react/create-simulation.tsx
@@ -5,33 +5,42 @@ import { createSimulationBase, OmitSimulation } from '../create-simulation-base'
 import type { IRenderableHooks, IRenderableMetaDataBase, Simulation } from '../types';
 import { baseRender } from '../create-renderable-base';
 
-export type OmitReactSimulation<DATA extends IReactSimulation> = Omit<OmitSimulation<DATA>, 'renderer' | 'cleanup'>
+export type OmitReactSimulation<DATA extends IReactSimulation> = Omit<OmitSimulation<DATA>, 'renderer' | 'cleanup'>;
 
-export function createSimulation<COMP extends React.ComponentType<any>>(input: OmitReactSimulation<IReactSimulation<COMP>>): IReactSimulation<COMP> {
+export function createSimulation<COMP extends React.ComponentType<any>>(
+    input: OmitReactSimulation<IReactSimulation<COMP>>
+): IReactSimulation<COMP> {
     const res: IReactSimulation<COMP> = createSimulationBase<IReactSimulation<COMP>>({
         ...input,
         renderer(target) {
-            baseRender(res, () => {
-                let element = this.wrapper ? <this.wrapper renderSimulation={(props) => {
-                    return <res.target {...(this.props as any)} {...props} />
-                }} /> : <res.target {...(this.props as any)} />;
-                const wrapRenderPlugins = getPluginsWithHooks(res, 'wrapRender')
-                for (const plugin of wrapRenderPlugins) {
-                    const el = plugin.key.plugin?.wrapRender!(plugin.props, res, element, target);
-                    element = el || element;
-                }
-                ReactDOM.render(element, target)
-            }, target)
-
+            baseRender(
+                res,
+                () => {
+                    let element = this.wrapper ? (
+                        <this.wrapper
+                            renderSimulation={(props) => {
+                                return <res.target {...(this.props as any)} {...props} />;
+                            }}
+                        />
+                    ) : (
+                        <res.target {...(this.props as any)} />
+                    );
+                    const wrapRenderPlugins = getPluginsWithHooks(res, 'wrapRender');
+                    for (const plugin of wrapRenderPlugins) {
+                        const el = plugin.key.plugin?.wrapRender!(plugin.props, res, element, target);
+                        element = el || element;
+                    }
+                    ReactDOM.render(element, target);
+                },
+                target
+            );
         },
         cleanup(target) {
             ReactDOM.unmountComponentAtNode(target);
-        }
-
-    })
-    return res
+        },
+    });
+    return res;
 }
-
 
 export interface ISimulationWrapperProps<P> {
     /**
@@ -42,9 +51,21 @@ export interface ISimulationWrapperProps<P> {
 }
 
 export interface IReactSimulationHooks extends IRenderableHooks {
-    wrapRender?: (props: any, renderable: IRenderableMetaDataBase, renderableElement: JSX.Element, canvas: HTMLElement) => null | JSX.Element,
+    wrapRender?: (
+        props: any,
+        renderable: IRenderableMetaDataBase,
+        renderableElement: JSX.Element,
+        canvas: HTMLElement
+    ) => null | JSX.Element;
 }
-export interface IReactSimulation<ComponentType extends React.ComponentType = React.ComponentType<any>> extends Simulation<ComponentType, Partial<React.PropsWithChildren<ComponentType extends React.ComponentType<infer P> ? P : {}>>, IReactSimulationHooks> {
+export interface IReactSimulation<ComponentType extends React.ComponentType = React.ComponentType<any>>
+    extends Simulation<
+        ComponentType,
+        Partial<
+            React.PropsWithChildren<ComponentType extends React.ComponentType<infer P> ? P : Record<string, unknown>>
+        >,
+        IReactSimulationHooks
+    > {
     /** The simulated component type. */
     componentType: ComponentType;
 
@@ -56,20 +77,23 @@ export interface IReactSimulation<ComponentType extends React.ComponentType = Re
      */
     // TODO - change props to be optional field (props?: ...)
 
-
     /**
      * Allows to wrap the simulated component in another component. Useful for providing context,
      * rendering controlled components, or rendering the simulated component multiple times - for
      * example a radio button as a radio group.
      */
-    wrapper?: React.FunctionComponent<ISimulationWrapperProps<ComponentType extends React.ComponentType<infer P> ? P : {}>>;
+    wrapper?: React.FunctionComponent<
+        ISimulationWrapperProps<ComponentType extends React.ComponentType<infer P> ? P : Record<string, unknown>>
+    >;
 }
 
 /**
  * @deprecated
  * use IReactSimulation type instead
  */
-export type ISimulation<ComponentType extends React.ComponentType = React.ComponentType<any>> = IReactSimulation<ComponentType>
+export type ISimulation<
+    ComponentType extends React.ComponentType = React.ComponentType<any>
+> = IReactSimulation<ComponentType>;
 
 /**
  * @deprecated
