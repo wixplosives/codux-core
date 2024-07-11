176cd37b40a6d615802801a4f58ee3f0b6799cab
NadavWix <nadav@wix.com>
2024-07-11 20:15:14 +0300

    work on APIs

--- packages/react-board/src/define-app-types.ts
+++ packages/react-board/src/define-app-types.ts
@@ -1,10 +1,71 @@
-import { IDirectoryContents, IFileSystem } from '@file-services/types';
-import { IRenderableMetadataBase } from '@wixc3/board-core';
+import { IFileSystem } from '@file-services/types';
 
-export interface DirChildInfo {
-    isFile: boolean;
-    name: string;
-    fullPath: string;
+export interface IReactApp<T = unknown> {
+    /**
+     * Should be isomorphic, should return the same result on the server and in a web worker
+     * returns the app manifest containing a list of routes for the app.
+     *
+     * should call onManifestUpdate when the manifest is updated
+     */
+    prepareApp: (options: IPrepareAppOptions) => Promise<{
+        manifest: IServerAppManifest<T>;
+        dispose: () => void;
+    }>;
+    /**
+     *
+     * Should be isomorphic, should return the same result on the server and in a web worker
+     *
+     * returns the information needed to create a new page
+     *
+     */
+    getNewPageInfo?: (options: IGetNewPageInfoOptions<T>) => {
+        isValid: boolean;
+        errorMessage?: string;
+        pageModule: string;
+        newPageSourceCode: string;
+    };
+
+    App: React.ComponentType<IReactAppProps<T>>;
+    /**
+     * Renders the App into an HTML element
+     *
+     * @returns a cleanup function
+     */
+    render: (targetElement: HTMLElement, props: IReactAppProps<T>) => Promise<() => void>;
+}
+export interface IAppManifest<T = unknown> {
+    routes: RouteInfo<T>[];
+    homeRoute: PageInfo<T>;
+    errorRoute?: PageInfo<T>;
+}
+
+export interface PageInfo<T = unknown> {
+    pageModule: string;
+    pageExportName?: string;
+    /**
+     * any parent layouts that should be editable in codux above this page
+     */
+    parentLayouts?: Array<{
+        layoutModule: string;
+        layoutExportName?: string;
+        editablePoints?: EditablePointOfInterest[];
+    }>;
+    /**
+     * a list of export names of the page that should be editable
+     * if the page is a function, the UI will edit its return value
+     * if the page is an object, the UI will edit the object
+     *
+     * if a schema is not provided, the UI will try to infer the schema from the code
+     */
+    editablePoints?: EditablePointOfInterest[];
+    /**
+     * any extra data that should be passed to the App component
+     */
+    extraData: T;
+}
+
+export interface RouteInfo<T = unknown> extends PageInfo<T> {
+    path: Array<StaticRoutePart | DynamicRoutePart>;
 }
 
 export interface StaticRoutePart {
@@ -12,78 +73,73 @@ export interface StaticRoutePart {
     text: string;
 }
 
-export interface NamedOption {
-    name: string;
-    text: string;
-}
 export interface DynamicRoutePart {
     kind: 'dynamic';
     name: string;
 }
-export interface RouteInfo<T = unknown> {
-    isPage?: boolean;
-    path: Array<StaticRoutePart | DynamicRoutePart>;
-    pageModule: string;
-    pageExportName?: string;
-    parentLayouts?: Array<{
-        layoutModule: string;
-        layoutExportName?: string;
-    }>;
-    extraData: T;
-}
-export interface IReactAppManifest<T = unknown> {
-    routes: RouteInfo<T>[];
-}
 
 export interface IReactAppProps<T = unknown> {
-    manifest: IReactAppManifest<T>;
-    requireModule: (filePath: string) => {
-        moduleResults: Promise<IResults<unknown>>;
-        dispose: () => void;
-    };
+    manifest: IAppManifest<T>;
+    requireModule: RequireModule;
     uri: string;
     setUri: (uri: string) => void;
 }
 
-export interface IAppManifest {
-    routes: RouteInfo[];
+export type RequireModule = (
+    filePath: string,
+    onModuleChange?: (updatedModule: IResults<unknown>) => void,
+) => {
+    moduleResults: Promise<IResults<unknown>>;
+    dispose: () => void;
+};
+
+export interface IServerPageApi<T> extends PageInfo<T> {
+    /**
+     * a list of all the urls that this route can be accessed from
+     * for example if the route is /users/:userId
+     * the urls could be /users/1, /users/2 etc
+     *
+     * @returns a list of all the urls that this route can be accessed from
+     */
+    getDynamicRouteURIs?: () => Promise<TitledURI[]>;
+}
+
+export interface IServerPageInfo<T> extends PageInfo<T>, IServerPageApi<T> {}
+
+export interface IServerRouteInfo<T> extends RouteInfo<T>, IServerPageApi<T> {}
+
+export interface IServerAppManifest<T = unknown> {
+    routes: IServerRouteInfo<T>[];
+    homeRoute: IServerPageInfo<T>;
+    errorRoute?: IServerPageInfo<T>;
 }
 
 export interface IPrepareAppOptions {
-    onAppUpdate: (appProps: IAppManifest) => void;
+    onManifestUpdate: (appProps: IServerAppManifest) => void;
     fs: IFileSystem;
 }
 export interface IGetNewPageInfoOptions<T> {
     fs: IFileSystem;
     wantedPath: RouteInfo['path'];
-    manifest: IReactAppManifest<T>;
+    manifest: IAppManifest<T>;
 }
-export interface IReactApp<T = unknown> extends IRenderableMetadataBase {
-    /** An image URL to be used as the board's thumbnail. */
-    cover?: string;
 
-    /** A list of tags. */
-    tags?: string[];
-
-    App: React.ComponentType<IReactAppProps<T>>;
-
-    setProps: (props: IReactAppProps<T>) => void;
-    prepareApp: (options: IPrepareAppOptions) => Promise<{
-        manifest: IReactAppManifest<T>;
-        dispose: () => void;
-    }>;
-    getNewPageInfo?: (options: IGetNewPageInfoOptions<T>) => {
-        pagePath: string;
-        contents: string | IDirectoryContents;
-    };
-    getRouteUrls?: (route: RouteInfo<T>) => string[];
-    bookmarks?: string[];
+export interface EditablePointOfInterest {
+    title: string;
+    exportName: string;
+    /** JSON SCHEMA for the editable point, for a method, the schema for the return value */
+    schema?: any;
 }
 
 export interface IResults<T> {
     status: 'ready' | 'invalid' | 'disposed';
     results: T | null;
     errorMessage?: string;
+}
+
+export interface TitledURI {
+    name: string;
+    uri: string;
 }
 
 export type OmitReactApp<T extends IReactApp<D>, D> = Omit<T, 'render' | 'setupStage' | 'setProps'>;
--- packages/react-board/src/define-app.tsx
+++ packages/react-board/src/define-app.tsx
@@ -1,34 +1,17 @@
 import React from 'react';
 import { reactErrorHandledRendering } from './react-error-handled-render';
-import { IReactApp, IReactAppProps, OmitReactApp } from './define-app-types';
+import { IReactApp, OmitReactApp } from './define-app-types';
 
 export function defineApp<T>(input: OmitReactApp<IReactApp<T>, T>): IReactApp<T> {
-    let appProps: IReactAppProps<T>;
     const res: IReactApp<T> = {
         ...input,
-        setProps(props) {
-            appProps = props;
-        },
-        async render(target) {
+
+        async render(target, appProps) {
             if (!appProps) {
                 throw new Error('AppProperties must be set using set props before calling render');
             }
             const element = <res.App {...appProps} />;
             return reactErrorHandledRendering(element, target);
-        },
-        setupStage(parentElement) {
-            const container = document.createElement('div');
-            container.style.display = 'contents';
-            const attachTo = parentElement || document.body;
-            attachTo.appendChild(container);
-            return {
-                canvas: container,
-                cleanup() {
-                    container.remove();
-                },
-                updateWindow() {},
-                updateCanvas() {},
-            };
         },
     };
     return res;
