9c959706be59b85a418db8548aca798ba729d883
Sagiv Dayan <sagiv.keeper@gmail.com>
2024-07-30 15:17:35 +0300

    fix: return `data-content-slot` attribute always - empty string if no name provided

--- packages/react-board/src/content-slot.tsx
+++ packages/react-board/src/content-slot.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export function ContentSlot(props: { children?: React.ReactNode; name?: string }) {
     return (
-        <div data-content-slot={props.name} style={{ display: 'contents' }}>
+        <div data-content-slot={props.name || ''} style={{ display: 'contents' }}>
             {props.children}
         </div>
     );
