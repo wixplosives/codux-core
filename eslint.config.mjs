a655fe8a252f25a1ef89cfca2727f9801fee62fc
Avi Vahl <avi.vahl@wix.com>
2024-08-04 15:21:55 +0300

    infra: migrate to eslint@9

---
+++ eslint.config.mjs
@@ -0,0 +1,40 @@
+import { fixupPluginRules } from "@eslint/compat";
+import pluginJs from "@eslint/js";
+import configPrettier from "eslint-config-prettier";
+import pluginNoOnlyTests from "eslint-plugin-no-only-tests";
+import pluginReact from "eslint-plugin-react";
+import pluginReactHooks from "eslint-plugin-react-hooks";
+import pluginTypescript from "typescript-eslint";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { ignores: ["**/dist/", "**/*.{js,mjs,cjs}"] },
+  pluginJs.configs.recommended,
+
+  ...pluginTypescript.configs.recommendedTypeChecked,
+  { languageOptions: { parserOptions: { projectService: true } } },
+
+  pluginReact.configs.flat.recommended,
+  // pluginReact.configs.flat["jsx-runtime"],
+  { settings: { react: { version: "detect" } } },
+
+  {
+    plugins: {
+      "react-hooks": pluginReactHooks,
+      "no-only-tests": fixupPluginRules(pluginNoOnlyTests),
+    },
+  },
+  configPrettier,
+
+  {
+    rules: {
+      "no-console": "error",
+      "react/prop-types": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "error",
+      "no-only-tests/no-only-tests": "error",
+      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }], // allow unused _ variables
+      "@typescript-eslint/unbound-method": "off",
+    },
+  },
+];
--- pleb.config.mjs
+++ pleb.config.mjs
@@ -2,6 +2,5 @@ export default {
   pinnedPackages: [
     { name: 'chai', reason: 'v5 is pure esm' },
     { name: 'chai-as-promised', reason: 'v8 is pure esm' },
-    { name: 'eslint', reason: 'plugins are not yet compatible with v9' },
   ],
 };
