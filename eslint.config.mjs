d051ebeb058719163f88c980432b41bb3da36ac5
Avi Vahl <avi.vahl@wix.com>
2024-08-15 14:17:06 +0300

    infra: re-enable lint for js/mjs/cjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,40 +1,47 @@
-import { fixupPluginRules } from "@eslint/compat";
-import pluginJs from "@eslint/js";
-import configPrettier from "eslint-config-prettier";
-import pluginNoOnlyTests from "eslint-plugin-no-only-tests";
-import pluginReact from "eslint-plugin-react";
-import pluginReactHooks from "eslint-plugin-react-hooks";
-import pluginTypescript from "typescript-eslint";
+import { fixupPluginRules } from '@eslint/compat';
+import pluginJs from '@eslint/js';
+import configPrettier from 'eslint-config-prettier';
+import pluginNoOnlyTests from 'eslint-plugin-no-only-tests';
+import pluginReact from 'eslint-plugin-react';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import pluginTypescript from 'typescript-eslint';
+
+for (const config of pluginTypescript.configs.recommendedTypeChecked) {
+  config.files = ['**/*.{ts,tsx,mts,cts}']; // ensure config only targets TypeScript files
+}
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { ignores: ["**/dist/", "**/*.{js,mjs,cjs}"] },
+  { ignores: ['**/dist/'] },
   pluginJs.configs.recommended,
-
-  ...pluginTypescript.configs.recommendedTypeChecked,
-  { languageOptions: { parserOptions: { projectService: true } } },
-
   pluginReact.configs.flat.recommended,
   // pluginReact.configs.flat["jsx-runtime"],
-  { settings: { react: { version: "detect" } } },
-
+  { settings: { react: { version: 'detect' } } },
   {
     plugins: {
-      "react-hooks": pluginReactHooks,
-      "no-only-tests": fixupPluginRules(pluginNoOnlyTests),
+      'react-hooks': pluginReactHooks,
+      'no-only-tests': fixupPluginRules(pluginNoOnlyTests),
+    },
+  },
+  {
+    rules: {
+      'no-console': 'error',
+      'no-only-tests/no-only-tests': 'error',
+      'no-undef': 'off',
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+      'react/prop-types': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'error',
+    },
+  },
+  ...pluginTypescript.configs.recommendedTypeChecked,
+  { languageOptions: { parserOptions: { projectService: true } } },
+  {
+    files: ['**/*.{ts,tsx,mts,cts}'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+      '@typescript-eslint/unbound-method': 'off',
     },
   },
   configPrettier,
-
-  {
-    rules: {
-      "no-console": "error",
-      "react/prop-types": "off",
-      "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "error",
-      "no-only-tests/no-only-tests": "error",
-      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }], // allow unused _ variables
-      "@typescript-eslint/unbound-method": "off",
-    },
-  },
 ];
