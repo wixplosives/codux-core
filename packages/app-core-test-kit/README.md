07fa2e1a9688c31f65f2d66d26018f7c034273bd
NadavWix <nadav@wix.com>
2024-08-18 17:18:11 +0300

    started working on app definition test kit

---
+++ packages/app-core-test-kit/README.md
@@ -0,0 +1,7 @@
+# @wixc3/app-core-test-kit
+
+this package exposes utilities for testing app definitions
+
+## License
+
+MIT
---
+++ packages/app-core-test-kit/src/index.ts
@@ -0,0 +1,66 @@
+import { FSApi, IAppManifest, IReactApp } from '@wixc3/app-core';
+import path from 'path';
+export interface AppDefDriverOptions {
+    app: IReactApp;
+    initialFiles: Record<
+        string,
+        {
+            contents: string;
+            exports: Set<string>;
+        }
+    >;
+    /**
+     * @default false
+     */
+    winFs?: boolean;
+    /**
+     * @default '/app-def.ts'
+     */
+    appDefFilePath?: string;
+    /**
+     * @default '/'
+     */
+    projectPath?: string;
+}
+
+export class AppDefDriver {
+    constructor(private options: AppDefDriverOptions) {}
+    private lastManifest: IAppManifest | null = null;
+    private disposeApp?: () => void;
+    async init() {
+        const { dispose, manifest } = await this.options.app.prepareApp({
+            fsApi: this.fsApi,
+            onManifestUpdate: (manifest) => {
+                this.lastManifest = manifest;
+            },
+        });
+        this.lastManifest = manifest;
+        this.disposeApp = dispose;
+        return manifest;
+    }
+    fsApi: FSApi = {
+        appDefFilePath: this.options.appDefFilePath || '/app-def.ts',
+        projectPath: this.options.projectPath || '/',
+        path: (this.options.winFs ? (path as { win32: FSApi['path'] }).win32 : path) as FSApi['path'],
+        watchDirectory: (dirPath: string) => {
+            return {
+                stop: () => {},
+                filePaths: Promise.resolve(
+                    Object.keys(this.options.initialFiles).filter((filePath) => filePath.startsWith(dirPath)),
+                ),
+            };
+        },
+        watchFile: (filePath: string) => {
+            return {
+                stop: () => {},
+                contents: Promise.resolve(this.options.initialFiles[filePath]?.contents || null),
+            };
+        },
+        watchFileExports: (filePath: string) => {
+            return {
+                stop: () => {},
+                exportNames: Promise.resolve([...(this.options.initialFiles[filePath]?.exports || [])]),
+            };
+        },
+    };
+}
