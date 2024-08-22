b65c10e987b063eec039b45e832f0e62f51781a1
Avi Vahl <avi@vahl.co.il>
2024-08-22 22:49:27 +0300

    feat!: drop support for react 17 and allow using newer RC versions of 19

--- packages/react-board/src/react-error-handled-render.tsx
+++ packages/react-board/src/react-error-handled-render.tsx
@@ -1,57 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import ReactDOMClient from 'react-dom/client';
 
 const reactRootByContainer = new WeakMap<HTMLElement, ReactDOMClient.Root>();
 
 export const reactErrorHandledRendering = async (element: React.ReactElement, container: HTMLElement) => {
-    if (ReactDOMClient.createRoot) {
-        // react 18+
-        const reactRoot = reactRootByContainer.get(container) || ReactDOMClient.createRoot(container);
-        reactRootByContainer.set(container, reactRoot);
+    const reactRoot = reactRootByContainer.get(container) || ReactDOMClient.createRoot(container);
+    reactRootByContainer.set(container, reactRoot);
 
-        const cleanup = () => {
-            reactRoot?.unmount();
-            reactRootByContainer.delete(container);
-        };
+    const cleanup = () => {
+        reactRoot.unmount();
+        reactRootByContainer.delete(container);
+    };
 
-        try {
-            await new Promise<void>((resolve, reject) => {
-                reactRoot?.render(
-                    <ErrorBoundary onRender={resolve} reportError={reject}>
-                        {element}
-                    </ErrorBoundary>,
-                );
-            });
-        } catch (e) {
-            cleanup();
-            throw e;
-        }
-
-        return cleanup;
-    } else {
-        // react <18
-        // eslint-disable-next-line react/no-deprecated
-        const cleanup = () => ReactDOM.unmountComponentAtNode(container);
-
-        try {
-            await new Promise<void>((resolve, reject) => {
-                // eslint-disable-next-line react/no-deprecated
-                ReactDOM.render(<ErrorBoundary reportError={reject}>{element}</ErrorBoundary>, container, resolve);
-            });
-        } catch (e) {
-            /**
-             * If an error occurs during ReactDOM.render(), React 17 will keep
-             * _reactRootContainer property attached to the container DOM node.
-             * This property points to an a stale fiber object, which prevents
-             * subsequent ReactDOM.render() calls from working.
-             */
-            cleanup();
-            throw e;
-        }
-
-        return cleanup;
+    try {
+        await new Promise<void>((resolve, reject) => {
+            reactRoot.render(
+                <ErrorBoundary onRender={resolve} reportError={reject}>
+                    {element}
+                </ErrorBoundary>,
+            );
+        });
+    } catch (e) {
+        cleanup();
+        throw e;
     }
+
+    return cleanup;
 };
 
 interface ErrorBoundryProps {
