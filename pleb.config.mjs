2db88bcd4b66387e3dd96d26e8092df59749f306
Avi Vahl <avi@vahl.co.il>
2024-08-31 17:49:11 +0300

    chore(deps-dev): upgrade to latest stable and regenerate lock file

--- pleb.config.mjs
+++ pleb.config.mjs
@@ -2,5 +2,6 @@ export default {
   pinnedPackages: [
     { name: 'chai', reason: 'v5 is pure esm' },
     { name: 'chai-as-promised', reason: 'v8 is pure esm' },
+    { name: '@types/chai-as-promised', reason: 'v8 is pure esm' },
   ],
 };
