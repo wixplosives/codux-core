db9e79ce3eb1bf6fefebb005a9d0775af1b84b20
PeterShershov <stelaviv@gmail.com>
2024-10-06 09:32:23 +0300

    fix(define-remix-app): fix page template to include an import to the LoaderFunctionArgs interface

--- packages/define-remix-app/src/page-template.ts
+++ packages/define-remix-app/src/page-template.ts
@@ -10,22 +10,21 @@ return <div>${clearJsxSpecialCharactersFromText(pageName)}</div>;
         : `
 import React from 'react';
 import { useLoaderData } from "@remix-run/react";
+import type { LoaderFunctionArgs } from '@remix-run/node';
 
-export const loader = async (params: { 
-${[...varNames].map((name) => `${name}: string`).join(',\n')}
-}) => {
-return params;
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+    return params;
 };
 
 const ${compIdentifier} = () => {
-const params = useLoaderData<typeof loader>();
-return <div>
-${[...varNames].map((name) => `<div>${clearJsxSpecialCharactersFromText(name)}: {params["${name}"]}</div>`).join('\n')}
-</div>;
+    const params = useLoaderData<typeof loader>();
+    return <div>
+        ${[...varNames].map((name) => `<div>${clearJsxSpecialCharactersFromText(name)}: {params["${name}"]}</div>`).join('\n')}
+        </div>;
 };
+
 export default ${compIdentifier};
   
-        
         `;
 };
 
