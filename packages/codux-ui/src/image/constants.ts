9c7dbedef2d626555b37737f7c2d7b3772ae5cdc
0xRezaa <rezab@wix.com>
2024-11-06 13:12:35 +0100

    feat(codux-ui): create new package codux-ui This PR creates a new package in codux-core called codux-ui (@wixc3/codux-ui). It will contain UI components which integrate well with codux. For the beginning it rolls out with a image component which handles wix media images and handles responsive design.

---
+++ packages/codux-ui/src/image/constants.ts
@@ -0,0 +1,3 @@
+export const WIX_MEDIA_STORAGE_URL = 'https://static.wixstatic.com';
+export const WIX_MEDIA_ROOT = `${WIX_MEDIA_STORAGE_URL}/media/`;
+export const WIX_MEDIA_ID_ROOT = 'wix:image://v1/';
