25a053fa3e51330a721a4d7fba374196337d73bd
Nadav Abrahami <nadav@wix.com>
2024-11-11 10:10:39 +0200

    fixed nav to home and made nav more readable

--- packages/define-remix-app/src/define-remix-app.tsx
+++ packages/define-remix-app/src/define-remix-app.tsx
@@ -9,7 +9,7 @@ import {
     IResults,
     RoutingPattern,
 } from '@wixc3/app-core';
-import { useMemo, useRef, useEffect } from 'react';
+import { useMemo, useEffect } from 'react';
 import {
     anErrorRoute,
     aRoute,
@@ -59,7 +59,6 @@ export const INVALID_MSGS = {
 export default function defineRemixApp({ appPath, routingPattern = 'file' }: IDefineRemixAppProps) {
     let rootLayouts: RouteExtraInfo['parentLayouts'] = [];
     let layoutMap: Map<string, ParentLayoutWithExtra> = new Map();
-    const navigation = new Navigation();
     const getRouteLayouts = (filePathInRouteDir: string, fsApi: FSApi, layouts = layoutMap) => {
         const parentLayouts: RouteExtraInfo['parentLayouts'] = [...rootLayouts];
         const routeLayouts = filePathToLayoutMatching(filePathInRouteDir, fsApi.path);
@@ -198,6 +197,7 @@ export default function defineRemixApp({ appPath, routingPattern = 'file' }: IDe
             },
         };
     };
+    const navigation = new Navigation();
     return defineApp<RouteModuleInfo, undefined>({
         App: ({
             manifest,
@@ -207,20 +207,10 @@ export default function defineRemixApp({ appPath, routingPattern = 'file' }: IDe
             onCaughtError,
             callServerMethod,
         }: IReactAppProps<RouteModuleInfo, undefined>) => {
-            const uriRef = useRef(uri);
-            uriRef.current = uri;
+            navigation.setOnPreviewNavigate(setUri);
             const { Router } = useMemo(
-                () =>
-                    manifestToRouter(
-                        manifest,
-                        navigation,
-                        importModule,
-                        setUri,
-                        onCaughtError,
-                        uriRef,
-                        callServerMethod,
-                    ),
-                [manifest, importModule, setUri, onCaughtError, callServerMethod],
+                () => manifestToRouter(manifest, navigation, importModule, onCaughtError, callServerMethod),
+                [manifest, importModule, onCaughtError, callServerMethod],
             );
 
             useEffect(() => {
--- packages/define-remix-app/src/navigation.ts
+++ packages/define-remix-app/src/navigation.ts
@@ -2,13 +2,28 @@ import { NavigateFunction } from '@remix-run/react';
 
 export class Navigation {
     private navigateFunction?: NavigateFunction;
+    private onPreviewNavigate?: (path: string) => void;
+    private currentPath: string = '';
+    constructor() {}
 
     navigate(path: string) {
+        this.currentPath = path;
         this.navigateFunction?.(path);
     }
 
+    onPreviewNavigation(path: string) {
+        this.currentPath = path;
+        this.onPreviewNavigate?.(path);
+    }
+
+    setOnPreviewNavigate(onPreviewNavigate: (path: string) => void) {
+        this.onPreviewNavigate = onPreviewNavigate;
+    }
     setNavigateFunction(navigateFunction: NavigateFunction) {
         this.navigateFunction = navigateFunction;
     }
-}
 
+    getCurrentPath() {
+        return this.currentPath;
+    }
+}
