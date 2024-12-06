36d987028d2fa4e6ef92d62ce8e77ec7e4a707c7
Avi Vahl <avi@vahl.co.il>
2024-12-07 01:12:59 +0200

    infra: ignore react@19 and typescript@5.7 until remix is compatible

--- pleb.config.mjs
+++ pleb.config.mjs
@@ -1,5 +1,11 @@
 export default {
   pinnedPackages: [
+    { name: 'react', reason: `remix isn't compatible with react@19 yet` },
+    { name: 'react-dom', reason: `remix isn't compatible with react@19 yet` },
+    { name: '@types/react', reason: `remix isn't compatible with react@19 yet` },
+    { name: '@types/react-dom', reason: `remix isn't compatible with react@19 yet` },
+    { name: 'typescript', reason: 'remix breaks with typescript@5.7 (reported upstream)' },
+
     { name: 'chai', reason: 'esm-only' },
     { name: '@types/chai', reason: 'esm-only' },
     { name: 'chai-as-promised', reason: 'esm-only' },
