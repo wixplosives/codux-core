0fc4a8120b42b2bfd801c3f2cf4b9210947bb5cf
Avi Vahl <avi@vahl.co.il>
2025-01-09 01:38:21 +0200

    chore(deps-dev): typescript@5.7.3, latest others and regenerate lock file

--- pleb.config.mjs
+++ pleb.config.mjs
@@ -4,7 +4,6 @@ export default {
     { name: 'react-dom', reason: `remix isn't compatible with react@19 yet` },
     { name: '@types/react', reason: `remix isn't compatible with react@19 yet` },
     { name: '@types/react-dom', reason: `remix isn't compatible with react@19 yet` },
-    { name: 'typescript', reason: 'remix breaks with typescript@5.7 (reported upstream)' },
 
     { name: 'chai', reason: 'esm-only' },
     { name: '@types/chai', reason: 'esm-only' },
