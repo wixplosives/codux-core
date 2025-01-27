23bf54866df73d50657ca1d71ede17c828283049
serhiiz <serhiiz@wix.com>
2025-01-27 13:14:04 +0100

    bring back optimize dependencies since cjs is back

--- packages/vite-plugin-react-board/test-kit/fixtures/vite-test-project/vite.config.ts
+++ packages/vite-plugin-react-board/test-kit/fixtures/vite-test-project/vite.config.ts
@@ -4,4 +4,10 @@ import coduxPlugin from '@wixc3/vite-plugin-react-board';
 
 export default defineConfig({
     plugins: [react(), coduxPlugin()],
+    optimizeDeps: {
+        // because test project does not have own node modules there is a problem with optimizing this dependency
+        // and it need to be manually added. This happens only in test project without node modules.
+        // in real project with node_modules optimization works out of the box
+        include: ['@wixc3/react-board'],
+    },
 });
